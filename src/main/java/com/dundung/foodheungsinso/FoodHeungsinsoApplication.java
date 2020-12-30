49e354eee7de8830ad648def15f47a77d910dabe
JunSeong <ebseud6135@naver.com>
2020-12-30 11:54:23 +0900

    feat: springboot, vue 프로젝트 생성

---
+++ src/main/java/com/dundung/foodheungsinso/FoodHeungsinsoApplication.java
@@ -0,0 +1,13 @@
+package com.dundung.foodheungsinso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHeungsinsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodHeungsinsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dundung/foodheungsinso/FoodHeungsinsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dundung.foodheungsinso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHeungsinsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
