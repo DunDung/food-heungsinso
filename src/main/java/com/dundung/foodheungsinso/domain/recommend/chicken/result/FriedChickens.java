a5ccc6ff338b8ec14c8fe5cbb2d922a16774f433
JunSeong <ebseud6135@naver.com>
2021-01-14 16:02:41 +0900

    refactor: 이미지 경로 버그 수정

--- src/main/java/com/dundung/foodheungsinso/domain/recommend/chicken/result/FriedChickens.java
+++ src/main/java/com/dundung/foodheungsinso/domain/recommend/chicken/result/FriedChickens.java
@@ -1,7 +1,5 @@
 package com.dundung.foodheungsinso.domain.recommend.chicken.result;
 
-import com.dundung.foodheungsinso.domain.recommend.Recommend;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,15 +10,14 @@ public enum FriedChickens {
             "핫후라이드",
             "바삭한 후라이드의 끝판왕",
             "BHC",
-            "매콤함이 더해진 최고의 후라이드 치킨, 다른 후라이드 치킨은 먹을 필요 없어요.",
+            "다른 후라이드 치킨은 먹을 필요 없어요. 매콤함이 더해진 최고의 후라이드 치킨입니다.",
             "bhc_hot_fried.jpg")),
     BBQ_GOLD_OLIVE(new Chicken(
             "황금 올리브",
             "후라이드의 왕",
             "BBQ",
-            "후라이드 치킨의 왕, 다른 후라이드 치킨은 못 비벼요.",
-            "bbq_gold_olive.png"));
-
+            "더 물어볼 필요 없습니다. 후라이드의 왕 황금 올리브 드시면. 다른 후라이드는 못 먹어요.",
+            "bbq_gold_olive.jpg"));
 
     private final Chicken chicken;
 
