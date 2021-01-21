0c6fde179e884ee902189bddf10eda42c0fb1e53
JunSeong <ebseud6135@naver.com>
2021-01-21 16:22:21 +0900

    feat: 정적 이미지 캐싱 및 압축 설정 추가

--- src/main/java/com/dundung/foodheungsinso/domain/recommend/config/WebConfiguration.java
+++ src/main/java/com/dundung/foodheungsinso/domain/recommend/config/WebConfiguration.java
@@ -1,8 +1,16 @@
 package com.dundung.foodheungsinso.domain.recommend.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -10,5 +18,22 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry formatterRegistrar) {
         formatterRegistrar.addConverter(new StringToAnswersConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        for (StaticResourcePath path : StaticResourcePath.values()) {
+            ResourceHandlerRegistration resourceHandlerRegistration = registry.addResourceHandler(path.getPath());
+            resourceHandlerRegistration.addResourceLocations("classpath:/static/" + path.getDirectory() + "/")
+                    .setCacheControl(CacheControl.maxAge(60L * 60L * 24L * 365L, TimeUnit.SECONDS).cachePublic());
+        }
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(
+                new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/js/*", "/css/*", "/img/*");
+        return filterRegistrationBean;
     }
 }
